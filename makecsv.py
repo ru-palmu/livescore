40e39dc37c734267f2f3c8cedb7375044876e5aa
(ru) <222255200+ru-palmu@users.noreply.github.com>
2025-10-27 14:38:17 +0900

    upp

--- makecsv.py
+++ makecsv.py
@@ -16,6 +16,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 from common import is_excluded, ru_model
+import re
 
 
 def comma_formatter(x, pos):
@@ -58,6 +59,11 @@ def readJsons(fnames: list, xlim) -> dict:
     with open(fname, 'r', encoding='utf-8') as f:
       data = json.load(f)
     data['filename'] = fname
+    if 'date' not in data:
+      # ファイル名から日付を取得する
+      path = Path(fname)
+      date_str = path.parent.name
+      data['date'] = date_str
 
     gifts = np.array(data.get('gift', []))
     data['total_gift'] = gifts.sum()
@@ -138,23 +144,10 @@ def write_csv_row(writer, data: dict):
   writer.writerow(row)
 
 
-def write_scatter(fname: str, jsons: dict,
-                  plot_livescore: bool = True,
-                  plot_rate: bool = True,
-                  xlim=None, ylim=None, title: str = ''):
-
-  fig, ax1 = plt.subplots()
-
-  ax2 = ax1.twinx()
-
-  # カンマ区切りフォーマット
-  ax1.yaxis.set_major_formatter(FuncFormatter(comma_formatter))
-  ax1.xaxis.set_major_formatter(FuncFormatter(comma_formatter))
-
-  ax1.set_xlabel('Gift ')
-  ax1.set_ylabel('Live Score')
-  ax2.set_ylabel('Live Score / Gift')
-
+def get_sorted_xy(jsons: dict) -> list:
+  """(total_gift, livescore) のリストを取得し，
+  total_gift でソートして返す。
+  """
   xy = []
   for dirname, data_list in jsons.items():
     for data in data_list:
@@ -163,13 +156,72 @@ def write_scatter(fname: str, jsons: dict,
       total_gift = np.sum(gifts)
       if total_gift == 0:
         continue
-      xy.append([total_gift, livescore])
+      xy.append((total_gift, livescore, data,))
 
   # x でソートする
   xy = sorted(xy, key=lambda v: v[0])
+  return xy
 
+
+def slice_dimension_xy(xy: list, dimension) -> set:
+  """はねていないデータ群"""
   xinvalid = set()
-  for i, (total_gift, livescore) in enumerate(xy):
+  if not dimension:
+    return xinvalid
+  if dimension == "exclude":
+    return get_xyinvalid(xy)
+  if not isinstance(dimension, str):
+    raise Exception(f"invalid dimension type: {type(dimension)}")
+
+  if re.fullmatch(r'20\d\d[01]\d[0-3]\d', dimension):
+    # 日付で区切る
+    slicer = lambda d: d['date'] == dimension
+  elif re.fullmatch(r'>20\d\d[01]\d[0-3]\d', dimension):
+    date_limit = dimension[1:]
+    slicer = lambda d: d['date'] > date_limit
+  elif re.fullmatch(r'>=20\d\d[01]\d[0-3]\d', dimension):
+    date_limit = dimension[2:]
+    slicer = lambda d: d['date'] >= date_limit
+  elif re.fullmatch(r'score\d+', dimension):
+    score_limit = int(dimension[6:])
+    slicer = lambda d: int(d.get('livescore', 0)) == score_limit
+  elif re.fullmatch(r'>score\d+', dimension):
+    score_limit = int(dimension[6:])
+    slicer = lambda d: int(d.get('livescore', 0)) > score_limit
+  elif re.fullmatch(r'<score\d+', dimension):
+    score_limit = int(dimension[6:])
+    slicer = lambda d: int(d.get('livescore', 0)) < score_limit
+  elif re.fullmatch(r'[><=]?20\d\d[01]\d[0-3]\d-\d+', dimension):
+    parts = dimension.split('-')
+    if len(parts) != 2:
+      date_limit = int(parts[0][1:])
+    else:
+      date_limit = int(parts[0])
+    rank_limit = int(parts[1])
+    if dimension[0] == '>':
+      slicer = lambda d: (int(d['date']) == date_limit and
+                          d.get('rank', 0) > rank_limit)
+    elif dimension[0] == '<':
+      slicer = lambda d: (int(d['date']) == date_limit and
+                          d.get('rank', 0) < rank_limit)
+    else:  # '='
+      slicer = lambda d: (int(d['date']) == date_limit and
+                          d.get('rank', 0) == rank_limit)
+  else:
+    raise Exception(f"invalid dimension: {dimension}")
+
+  for i, (_, _, d) in enumerate(xy):
+    if 'date' not in d:
+      print(d)
+    if not slicer(d):
+      xinvalid.add(i)
+
+  return xinvalid
+
+
+def get_xyinvalid(xy: list) -> set:
+  xinvalid = set()
+  for i, (total_gift, livescore, _) in enumerate(xy):
     if is_excluded(total_gift, livescore):
       # 無効データ
       xinvalid.add(i)
@@ -180,53 +232,12 @@ def write_scatter(fname: str, jsons: dict,
       print("livescore / totalgift =", livescore / total_gift,)
       print("-3*total_gift/130000 + 3 =",
             -3 * total_gift / 130_000 + 3)
-      assert False
+  return xinvalid
 
-  # 散布図
-  x = [v[0] for v in xy]
-  y_livescore = [v[1] for v in xy]
-  if plot_livescore:
-    ax1.scatter(x, y_livescore,
-                label='Real data (' + str(len(xy)) + ' samples)',
-                color='#1f77b4', alpha=0.5, s=5)
 
-  if plot_rate:
-    xvalid = [xy[i] for i, v in enumerate(xy) if i not in xinvalid]
-    xx = [v[0] for v in xvalid]
-    y_livescore_per_gift = [v[1] / v[0] for v in xvalid]
-
-    ax2.scatter(xx, y_livescore_per_gift, label='Real score / gift',
-                color='#FFD700', alpha=0.5, s=3)
-
-    xx = [xy[i][0] for i in xinvalid]
-    y_livescore_per_gift_invalid = [xy[i][1] / xy[i][0] for i in xinvalid]
-    ax2.scatter(xx, y_livescore_per_gift_invalid,
-                color='#888888', alpha=0.5, s=3)
-
-  # 旧モデル
-  xmax = x[-1]
-  x = [0.1] + x + [xmax * 1.1]
-  if plot_livescore:
-    y = [3 * v for v in x]
-    ax1.plot(x, y, label='3x gift',
-             color='#d62728', linestyle='dashed')
-
-    # 新モデル
-    y = [ru_model(v) for v in x]
-    ax1.plot(x, y, label='(ru) model',
-             color='#2ca02c')
-
-  y = [ru_model(v) / v for v in x]
-  if plot_rate:
-    ax2.plot(x, y, label='(ru) model score / gift',
-             color='#2ca02c', linestyle='-.')
-
-  # x/y 軸を原点交差
-  # ax1.spines['left'].set_position('zero')
-  # ax1.spines['bottom'].set_position('zero')
-  # ax2.spines['bottom'].set_color('none')
-  # ax2.spines['left'].set_color('none')
-
+def set_xylim_ax1(ax1, xlim, ylim, xmax):
+  """左軸の x/y 軸範囲を設定する。
+  """
   if xlim:
     ax1.set_xlim(0, xlim)
     if not ylim:
@@ -238,10 +249,10 @@ def write_scatter(fname: str, jsons: dict,
   else:
     ax1.set_ylim(0, None)
     xlim = xmax
+  return xlim
 
-  ax2.set_ylim(2.4, 3.5)
-  ax2.set_yticks([2.6, 2.8, 3.0, 3.2, 3.4])
 
+def plot_rank_zones(ax2, xlim):
   obi = {
       # (min_+2, max_+6, color, max_+2, max_+4, xlim_max)
       'SS': (1, 300_000, 810_000, 1_800_000),
@@ -308,6 +319,97 @@ def write_scatter(fname: str, jsons: dict,
         verticalalignment='center',
     )
 
+
+def write_scatter(fname: str, jsons: dict,
+                  plot_livescore: bool = True,
+                  plot_rate: bool = True,
+                  xlim=None, ylim=None, title: str = '',
+                  dimension=None):
+
+  fig, ax1 = plt.subplots()
+
+  ax2 = ax1.twinx()
+
+  # カンマ区切りフォーマット
+  ax1.yaxis.set_major_formatter(FuncFormatter(comma_formatter))
+  ax1.xaxis.set_major_formatter(FuncFormatter(comma_formatter))
+
+  ax1.set_xlabel('Gift (Coin)')
+  ax1.set_ylabel('Live Score')
+  ax2.set_ylabel('Live Score / Gift')
+
+  xy = get_sorted_xy(jsons)
+  assert len(xy) > 0
+
+  xinvalid = slice_dimension_xy(xy, dimension)
+
+  # 散布図
+  x = [v[0] for v in xy]
+  y_livescore = [v[1] for v in xy]
+  if plot_livescore:
+    # 左軸：gift - livescore の散布図を描画する
+    ax1.scatter(x, y_livescore,
+                label='Real data (' + str(len(xy)) + ' samples)',
+                color='#1f77b4', alpha=0.5, s=5)
+
+  if plot_rate:
+    # 右軸：livescore / gift の散布図を描画する
+    xx = [xy[i][0] for i in xinvalid]
+    y_livescore_per_gift_invalid = [xy[i][1] / xy[i][0] for i in xinvalid]
+    ax2.scatter(xx, y_livescore_per_gift_invalid,
+                color='#888888', alpha=0.5, s=3)
+
+    xvalid = [xy[i] for i, v in enumerate(xy) if i not in xinvalid]
+    xx = [v[0] for v in xvalid]
+    y_livescore_per_gift = [v[1] / v[0] for v in xvalid]
+
+    label = 'Real score / gift'
+    if dimension:
+      label += f' ({dimension})'
+
+    if len(xx) < 10:
+      ax2.scatter(xx, y_livescore_per_gift, label=label,
+                  color='#FFCC00', alpha=0.9, s=10, marker='*')
+    else:
+      ax2.scatter(xx, y_livescore_per_gift, label=label,
+                  color='#FFCC00', alpha=0.5, s=3)
+
+  # 旧モデル
+  xmax = x[-1]
+  x = [0.1] + x + [xmax * 1.1]
+  if plot_livescore:
+    # 左軸：gift - livescore のモデル線を描画する
+    y = [3 * v for v in x]
+    ax1.plot(x, y, label='3x gift',
+             color='#d62728', linestyle='dashed')
+
+    # 新モデル
+    y = [ru_model(v) for v in x]
+    ax1.plot(x, y, label='(ru) model',
+             color='#2ca02c')
+
+  y = [ru_model(v) / v for v in x]
+  if plot_rate:
+    # 右軸：livescore / gift のモデル線を描画する
+    ax2.plot(x, y, label='(ru) model score / gift',
+             color='#2ca02c', linestyle='-.')
+
+  # x/y 軸を原点交差
+  # ax1.spines['left'].set_position('zero')
+  # ax1.spines['bottom'].set_position('zero')
+  # ax2.spines['bottom'].set_color('none')
+  # ax2.spines['left'].set_color('none')
+
+  xlim = set_xylim_ax1(ax1, xlim, ylim, xmax)
+
+  ax2.set_ylim(2.4, 3.5)
+  ax2.set_yticks([2.6, 2.8, 3.0, 3.2, 3.4])
+
+  # ==================================
+  # ランク帯塗りつぶし
+  # ==================================
+  plot_rank_zones(ax2, xlim)
+
   ax1.set_title(title)
   fig.legend(loc='upper center', bbox_to_anchor=(0.5, 0.93), ncol=1)
   ax1.grid(False)
@@ -324,6 +426,8 @@ def main():
   parser.add_argument('args', nargs='+', help="input json files")
   parser.add_argument('-f', help="output csv file; default STDOUT")
   parser.add_argument('--scatter', help="output scatter.png file")
+  parser.add_argument('--dimension', help="dimension to slice",
+                      default="", type=str)
   parser.add_argument('-x', '--xlim', type=int,
                       help="Maximum value for the x-axis limit")
   parser.add_argument('-y', '--ylim', type=int,
@@ -349,6 +453,9 @@ def main():
   # 引数解析
   # #############################
   jsons = readJsons(args.args, args.xlim)
+  if len(jsons) == 0:
+    print("no valid json data")
+    return 1
 
   # #############################
   # csv 出力
@@ -360,7 +467,8 @@ def main():
                   plot_livescore=not args.no_livescore,
                   plot_rate=not args.no_rate,
                   xlim=args.xlim, ylim=args.ylim,
-                  title=args.title if args.title else '')
+                  title=args.title if args.title else '',
+                  dimension=args.dimension)
 
 
 if __name__ == '__main__':
